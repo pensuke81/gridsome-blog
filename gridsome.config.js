65072028a8ca43f18b8888709eaf64c5e3432a52
pensuke <pensuke81@gmail.com>
2019-05-22 09:31:18 +0900

    install Tailwind CSS

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,3 +1,9 @@
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
+
 module.exports = {
   siteName: 'Gridsomeで作るブログ',
   plugins: [
@@ -24,5 +30,39 @@ module.exports = {
       externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
       anchorClassName: 'icon icon-link',
     }
+  },
+  
+  chainWebpack: config => {
+    config.module
+      .rule('css')
+      .oneOf('normal')
+      .use('postcss-loader')
+      .tap(options => {
+        options.plugins.unshift(...[
+          require('postcss-import'),
+          require('postcss-nested'),
+          require('tailwindcss'),
+        ])
+
+        if (process.env.NODE_ENV === 'production') {
+          options.plugins.push(...[
+            require('@fullhuman/postcss-purgecss')({
+              content: [
+                'src/assets/**/*.css',
+                'src/**/*.vue',
+                'src/**/*.js'
+              ],
+              extractors: [
+                {
+                  extractor: TailwindExtractor,
+                  extensions: ['css', 'vue', 'js']
+                }
+              ],
+            }),
+          ])
+        }
+
+        return options
+      })
   },
 }
